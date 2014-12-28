100aef100e46971ec91227913a26e3832bb2f16f
simmone <chenxiao770117@gmail.com>
2014-12-28 11:07:19 +0800

    Update

--- racket-simple-xlsx/test.sh
+++ racket-simple-xlsx/test.sh
@@ -1,8 +1,10 @@
-cd ~/racket-simple-xlsx/racket-simple-xlsx/lib/;racket lib-test.rkt
-cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test1/;racket test1-test.rkt
-cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test2/;racket test2-test.rkt
-cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test3/;racket test3-test.rkt
-cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test4/;racket test4-test.rkt
-cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test5/;racket test5-test.rkt
-cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test6/;racket test6-test.rkt
-cd ~/racket-simple-xlsx/racket-simple-xlsx/writer/;racket content-type-test.rkt
+echo "lib";cd ~/racket-simple-xlsx/racket-simple-xlsx/lib/;racket lib-test.rkt
+echo "reader-test1";cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test1/;racket test1-test.rkt
+echo "reader-test2";cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test2/;racket test2-test.rkt
+echo "reader-test3";cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test3/;racket test3-test.rkt
+echo "reader-test4";cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test4/;racket test4-test.rkt
+echo "reader-test5";cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test5/;racket test5-test.rkt
+echo "reader-test6";cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test6/;racket test6-test.rkt
+
+echo "writer-content-type";cd ~/racket-simple-xlsx/racket-simple-xlsx/writer/;racket content-type-test.rkt
+echo "writer-rels";cd ~/racket-simple-xlsx/racket-simple-xlsx/writer/;racket rels-test.rkt
---
+++ racket-simple-xlsx/writer/rels-test.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(provide test-rels)
+
+(require rackunit/text-ui)
+
+(require rackunit "rels.rkt")
+
+(define test-rels
+  (test-suite
+   "test-rels"
+
+   (test-case
+    "test-rels"
+    (check-equal? (write-rels)
+                  (string-append
+                   "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                   "<Relationships xmlns=\"http://schemas.openxmlformats.org/package/2006/relationships\"><Relationship Id=\"rId3\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties\" Target=\"docProps/app.xml\"/><Relationship Id=\"rId2\" Type=\"http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties\" Target=\"docProps/core.xml\"/><Relationship Id=\"rId1\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument\" Target=\"xl/workbook.xml\"/></Relationships>"))
+   )))
+
+(run-tests test-rels)
