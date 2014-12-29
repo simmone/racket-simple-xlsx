df36d7bf24982322a8aac162f233c10da0765d89
simmone <chenxiao770117@gmail.com>
2014-12-29 13:33:09 +0800

    Update

--- racket-simple-xlsx/test.sh
+++ racket-simple-xlsx/test.sh
@@ -1,11 +1,12 @@
-echo "lib";cd ~/racket-simple-xlsx/racket-simple-xlsx/lib/;racket lib-test.rkt
-echo "reader-test1";cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test1/;racket test1-test.rkt
-echo "reader-test2";cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test2/;racket test2-test.rkt
-echo "reader-test3";cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test3/;racket test3-test.rkt
-echo "reader-test4";cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test4/;racket test4-test.rkt
-echo "reader-test5";cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test5/;racket test5-test.rkt
-echo "reader-test6";cd ~/racket-simple-xlsx/racket-simple-xlsx/reader/tests/test6/;racket test6-test.rkt
+HOME_DIR="racket-simple-xlsx/racket-simple-xlsx"
+echo "lib";cd; cd $HOME_DIR/lib/;racket lib-test.rkt
+echo "reader-test1";cd; cd $HOME_DIR/reader/tests/test1/;racket test1-test.rkt
+echo "reader-test2";cd; cd $HOME_DIR/reader/tests/test2/;racket test2-test.rkt
+echo "reader-test3";cd; cd $HOME_DIR/reader/tests/test3/;racket test3-test.rkt
+echo "reader-test4";cd; cd $HOME_DIR/reader/tests/test4/;racket test4-test.rkt
+echo "reader-test5";cd; cd $HOME_DIR/reader/tests/test5/;racket test5-test.rkt
+echo "reader-test6";cd; cd $HOME_DIR/reader/tests/test6/;racket test6-test.rkt
 
-echo "writer-content-type";cd ~/racket-simple-xlsx/racket-simple-xlsx/writer/;racket content-type-test.rkt
-echo "writer-rels";cd ~/racket-simple-xlsx/racket-simple-xlsx/writer/;racket rels-test.rkt
-echo "writer-docprops-app";cd ~/racket-simple-xlsx/racket-simple-xlsx/writer/;racket docprops-app-test.rkt
+echo "writer-content-type";cd; cd $HOME_DIR/writer/;racket content-type-test.rkt
+echo "writer-rels";cd; cd $HOME_DIR/writer/;racket rels-test.rkt
+echo "writer-docprops-app";cd; cd $HOME_DIR/writer/;racket docprops-app-test.rkt
