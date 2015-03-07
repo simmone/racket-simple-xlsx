23e20a78f7929b087b272efe5c28009bd09df585
Chen Xiao <chenxiao770117@gmail.com>
2015-03-07 22:02:51 +0800

    Update

--- racket-simple-xlsx/test-writer.rkt
+++ racket-simple-xlsx/test-writer.rkt
@@ -2,5 +2,5 @@
 
 (require "writer.rkt")
 
-(let ([data_list '(((1 2 3) (4 5 6)) (("1" "2" "4") ("5")) (("1")))])
-  (write-xlsx-file data_list '("员工" "经理") "haha"))
+(let ([data_list '((("chenxiao")))])
+  (write-xlsx-file data_list #f "haha"))
