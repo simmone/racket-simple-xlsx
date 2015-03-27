bf2d1f01c8b7f4363ad38c667a9988879e28846c
Chen Xiao <chenxiao770117@gmail.com>
2015-03-27 11:10:18 +0800

    Update

--- info.rkt
+++ info.rkt
@@ -1,5 +1,13 @@
 #lang setup/infotab
 
+(define version "1.0")
+
 (define collection 'multi)
-(define deps '("base"))
-(define build-deps '("rackunit-lib"))
+
+(define deps '("base"
+               "rackunit-lib"
+               "at-exp-lib"))
+
+(define build-deps '("rackunit-lib"
+                     "at-exp-lib"))
+
