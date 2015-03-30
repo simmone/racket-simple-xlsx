54d5471bde76d0c49b13e287a34d908efde614d6
Chen Xiao <chenxiao770117@gmail.com>
2015-03-30 08:17:40 +0800

    Update

--- example/example-1.rkt
+++ example/example-1.rkt
@@ -8,9 +8,11 @@
   (let ([col_attr_hash (make-hash)])
     (hash-set! col_attr_hash "A" (col-attr 100 "red"))
     (hash-set! col_attr_hash "B" (col-attr 200 "blue"))
-    
+
     (send xlsx add-sheet '(("chenxiao" "陈晓") (1 2 34 100 456.34)) "Sheet1" #:col_attr_hash col_attr_hash)
     (send xlsx add-sheet '((1 2 3 4)) "Sheet2")
     (send xlsx add-sheet '(("a" "b")) "Sheet3")
+    (send xlsx add-sheet '(("a" "b")) "Sheet4" #:col_attr_hash col_attr_hash)
+    (send xlsx add-sheet '(("")) "Sheet5" #:col_attr_hash col_attr_hash)
     (write-xlsx-file xlsx "example1.xlsx")))
-  
+
