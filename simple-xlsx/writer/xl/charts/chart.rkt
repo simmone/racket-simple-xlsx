7025fa961b4368cee216d49d485f96fc2ba46867
simmone <chenxiao770117@gmail.com>
2016-11-24 20:42:10 +0800

    update

--- simple-xlsx/writer/xl/charts/chart.rkt
+++ simple-xlsx/writer/xl/charts/chart.rkt
@@ -39,7 +39,7 @@
                 (printf "<c:ser><c:idx val=\"~a\"/><c:order val=\"~a\"/>" ser_seq ser_seq)
                 (printf "<c:tx><c:v>~a</c:v></c:tx>" topic)
                 (printf "<c:marker><c:symbol val=\"none\"/></c:marker><c:val><c:numRef>")
-                (printf "<c:f>~a!~a</c:f>" sheet_name range_str)
+                (printf "<c:f>~a!~a</c:f>" sheet_name (convert-range range_str))
                 (printf "<c:numCache><c:formatCode>General</c:formatCode>")
                 (printf "<c:ptCount val=\"~a\"/>" (range-length range_str))
                 (let y-data-loop ([y_data_loop_list (send xlsx get-range-data sheet_name range_str)]
@@ -58,7 +58,7 @@
              [x_data_range (line-chart-sheet-x_data_range chart_sheet)]
              [y_data_range_list (line-chart-sheet-y_data_range_list chart_sheet)])
         (printf "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n")
-        (printf "<c:chartSpace xmlns:c=\"http://schemas.openxmlformats.org/drawingml/2006/chart\" xmlns:a=\"http://schemas.openxmlformats.org/drawingml/2006/main\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\"><c:lang val=\"zh-CN\"/><c:chart><c:title><c:tx><c:rich><a:bodyPr/><a:lstStyle/><a:p><a:pPr><a:defRPr/></a:pPr><a:r><a:rPr lang=\"zh-CN\" altLang=\"en-US\"/><a:t>@|topic|</a:t></a:r><a:r>/a:p></c:rich></c:tx><c:layout/></c:title><c:plotArea><c:layout/><c:lineChart><c:grouping val=\"standard\"/>")
+        (printf "<c:chartSpace xmlns:c=\"http://schemas.openxmlformats.org/drawingml/2006/chart\" xmlns:a=\"http://schemas.openxmlformats.org/drawingml/2006/main\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\"><c:lang val=\"zh-CN\"/><c:chart><c:title><c:tx><c:rich><a:bodyPr/><a:lstStyle/><a:p><a:pPr><a:defRPr/></a:pPr><a:r><a:rPr lang=\"zh-CN\" altLang=\"en-US\"/><a:t>~a</a:t></a:r></a:p></c:rich></c:tx><c:layout/></c:title><c:plotArea><c:layout/><c:lineChart><c:grouping val=\"standard\"/>" topic)
         (printf "~a" (print-x-data xlsx x_data_range))
         (printf "~a" (print-y-data-list xlsx y_data_range_list))
         (printf "<c:marker val=\"1\"/><c:axId val=\"76367360\"/><c:axId val=\"76368896\"/></c:lineChart><c:catAx><c:axId val=\"76367360\"/><c:scaling><c:orientation val=\"minMax\"/></c:scaling><c:axPos val=\"b\"/><c:numFmt formatCode=\"General\" sourceLinked=\"1\"/><c:majorTickMark val=\"none\"/><c:tickLblPos val=\"nextTo\"/><c:crossAx val=\"76368896\"/><c:crosses val=\"autoZero\"/><c:auto val=\"1\"/><c:lblAlgn val=\"ctr\"/><c:lblOffset val=\"100\"/></c:catAx><c:valAx><c:axId val=\"76368896\"/><c:scaling><c:orientation val=\"minMax\"/></c:scaling><c:axPos val=\"l\"/><c:majorGridlines/><c:numFmt formatCode=\"General\" sourceLinked=\"1\"/><c:majorTickMark val=\"none\"/><c:tickLblPos val=\"nextTo\"/><c:crossAx val=\"76367360\"/><c:crosses val=\"autoZero\"/><c:crossBetween val=\"between\"/></c:valAx></c:plotArea><c:legend><c:legendPos val=\"r\"/><c:layout/></c:legend><c:plotVisOnly val=\"1\"/></c:chart></c:chartSpace>")))))
