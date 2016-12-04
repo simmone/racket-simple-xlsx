e4ade3cb30c48a96d9b25da5be773e1b3e31b6a7
simmone <chenxiao770117@gmail.com>
2016-12-04 09:57:27 +0800

    update

--- simple-xlsx/tests/write-test/write-test.rkt
+++ simple-xlsx/tests/write-test/write-test.rkt
@@ -12,7 +12,14 @@
     "simple1"
               
     (let ([xlsx (new xlsx%)])
-      (send xlsx add-data-sheet "Sheet1" '((1 2 3 4 5)))
+      (send xlsx add-data-sheet "Sheet1" '(("201601" "201602" "201603" "201604" "201605" "201606" "201607" "201608" "201609" "201610" "201611" "201612")
+                                           (100 200 300 400 300 200 100 200 300 400 300 200)
+                                           (200 300 400 300 200 100 200 300 400 300 200 100)))
+
+      (send xlsx add-line-chart-sheet "Chart1" "TestChart")
+      (send xlsx set-line-chart-x-data! "Chart1" "Sheet1" "A1-L1")
+      (send xlsx set-line-chart-y-data! "Chart1" "Sheet1" "数据1" "A2-L2")
+      (send xlsx set-line-chart-y-data! "Chart1" "Sheet1" "数据2" "A3-L3")
 
       (write-xlsx-file xlsx "test1.xlsx")
       ))
