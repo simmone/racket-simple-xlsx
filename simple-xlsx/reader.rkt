480892d2c951234aa3a58486ca9978d5f1e9500f
Chen Xiao <chenxiao770117@gmail.com>
2016-12-20 17:04:03 +0800

    read bug fix, thank nano report.

--- simple-xlsx/reader.rkt
+++ simple-xlsx/reader.rkt
@@ -110,10 +110,20 @@
                   (let ([split_items2 (regexp-split #rx"<si>" (car split_items1))])
                     (when (> (length split_items2) 1)
                           (let* ([xml (xml->xexpr (document-element (read-xml (open-input-string (string-append "<si>" (second split_items2) "</si>")))))]
-                                 [v_list (xml-get-list 't xml)])
+                                 [v_list (xml-get-list 't xml)]
+                                 [ignore_rPh_map (make-hash)])
+
+                            (for-each
+                             (lambda (rPh_rec)
+                               (hash-set! ignore_rPh_map (third rPh_rec) ""))
+                             (xml-get-list 'rPh xml))
+                                 
                             (hash-set! data_map
                                        (number->string index)
-                                       (regexp-replace* #rx" " (foldr (lambda (a b) (string-append a b)) "" v_list) " ")))))
+                                       (regexp-replace* #rx" " 
+                                                        (foldr (lambda (a b) (string-append a b)) "" 
+                                                               (filter (lambda (item) (not (hash-has-key? ignore_rPh_map item))) v_list))
+                                                        " ")))))
                   (loop2 (cdr split_items1) (add1 index)))))))
   data_map))
 
