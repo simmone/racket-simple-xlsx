369474534c15f42c4eed41f2a4574e8686c0ba79
simmone <chenxiao770117@gmail.com>
2016-12-23 22:16:39 +0800

    update

--- simple-xlsx/writer/xl/worksheets/worksheet.rkt
+++ simple-xlsx/writer/xl/worksheets/worksheet.rkt
@@ -6,6 +6,7 @@
 (provide (contract-out
           [write-data-sheet (-> string? (is-a?/c xlsx%) string?)]
           [write-data-sheet-file (-> path-string? (is-a?/c xlsx%) void?)]
+          [get-col-width-map (-> (listof list?) hash?)]
           ))
 
 (define (write-data-sheet sheet_name xlsx)
@@ -24,6 +25,7 @@
         (let* ([sheet (send xlsx get-sheet-by-name sheet_name)]
                [data_sheet (sheet-content sheet)]
                [rows (data-sheet-rows data_sheet)]
+               [col_width_map (get-col-width-map rows)]
                [col_count (length (car rows))]
                [span_str (string-append "1:" (number->string col_count))]
                [width_hash (data-sheet-width_hash data_sheet)]
@@ -31,22 +33,29 @@
                [dimension (if (= (length rows) 0) "A1" (string-append "A1:" (get-dimension rows)))]
                [is_active (if (= (sheet-seq sheet) 1) "tabSelected=\"1\"" "")]
                [active_cell (if (null? data_sheet) "" "<selection activeCell=\"A1\" sqref=\"A1\"/>")])
-
+               
           (printf "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n")
           (printf "<worksheet xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\"><dimension ref=\"~a\"/><sheetViews><sheetView ~a workbookViewId=\"0\">~a</sheetView></sheetViews><sheetFormatPr defaultRowHeight=\"13.5\"/>" dimension is_active active_cell)
 
-          (when (> (hash-count width_hash) 0)
-                (printf "<cols>")
-                
-                (hash-for-each
-                 width_hash
-                 (lambda (col_range width)
-                   (let* ([items (regexp-match* #rx"([A-Z]+)" col_range)]
-                          [start_index (abc->number (first items))]
-                          [end_index (abc->number (second items))])
+          (hash-for-each
+           width_hash
+           (lambda (col_range width)
+             (let* ([items (regexp-match* #rx"([A-Z]+)" col_range)]
+                    [start_index (abc->number (first items))]
+                    [end_index (abc->number (second items))])
+               (let loop ([loop_index start_index])
+                 (when (<= loop_index end_index)
+                       (hash-set! col_width_map loop_index width)
+                       (loop (add1 loop_index)))))))
 
-                     (printf "<col min=\"~a\" max=\"~a\" width=\"~a\"/>" start_index end_index width))))
-                (printf "</cols>"))
+          (printf "<cols>")
+
+          (for-each
+           (lambda (col)
+             (printf "<col min=\"~a\" max=\"~a\" width=\"~a\"/>" (car col) (car col) (cdr col)))
+           (sort (hash->list col_width_map) < #:key car))
+           
+          (printf "</cols>")
           
           (printf "<sheetData>")
           
@@ -93,4 +102,23 @@
                   (lambda ()
                     (printf "~a" (write-data-sheet (sheet-name (car loop_list)) xlsx)))))
           (loop (cdr loop_list)))))
+
+(define (get-col-width-map rows)
+  (let ([col_width_map (make-hash)])
+    (let loop-row ([loop_rows rows])
+      (when (not (null? loop_rows))
+            (let loop-col ([cols (car loop_rows)]
+                           [index 1])
+              (when (not (null? cols))
+                    (let* ([str (if (number? (car cols)) (number->string (car cols)) (car cols))]
+                           [str_len (string-length str)]
+                           [bytes_len (bytes-length (string->bytes/utf-8 str))]
+                           [result_len (+ 2 (- bytes_len (floor (/ (- bytes_len str_len) 2))))])
+                      (when (< (hash-ref col_width_map index 0) result_len)
+                            (hash-set! col_width_map index result_len)))
+                    (loop-col (cdr cols) (add1 index))))
+            (loop-row (cdr loop_rows))))
+    col_width_map))
+                      
+                            
 
