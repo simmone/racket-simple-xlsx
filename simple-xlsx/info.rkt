e2d4a8e0753b628fc13e239b1f9c1bc15953d6be
Chen Xiao <chenxiao770117@gmail.com>
2019-09-15 16:30:01 +0800

    update

--- simple-xlsx/info.rkt
+++ simple-xlsx/info.rkt
@@ -3,7 +3,7 @@
 (define scribblings
   '(("simple-xlsx.scrbl" (multi-page) (tool 100))))
 
-(define compile-omit-paths '("test.sh" "tests"))
-(define test-omit-paths '("test.sh" "tests"))
+(define compile-omit-paths '("tests"))
+(define test-omit-paths '("tests"))
 
 
