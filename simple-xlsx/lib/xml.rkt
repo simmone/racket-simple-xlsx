f93f646dc742a8573c7f8aa0dc51b42c658b9e45
Chen Xiao <chenxiao770117@gmail.com>
2021-01-03 11:14:22 +0800

    update

--- simple-xlsx/lib/xml.rkt
+++ simple-xlsx/lib/xml.rkt
@@ -3,54 +3,92 @@
 (require xml)
 (require xml/xexpr)
 
+(require detail)
+
 (provide (contract-out
           [load-xml-hash (-> path-string? (listof symbol?) hash?)]
           ))
 
 (define (load-xml-hash xml sym_list)
-  (call-with-input-file
-   xml
-   (lambda (origin_port)
-     (call-with-input-string
-      (regexp-replace* #rx"> *<"
-                       (regexp-replace* #rx"\n|\r" (port->string origin_port) "")
-                       "><")
-      (lambda (filtered_port)
-        (let ([xml_hash (make-hash)]
-              [sym_hash (make-hash)]
-              )
+  (detail
+   #:formats? '("xml_decode.pdf")
+   (lambda ()
+     (detail-page
+      #:line_break_length? 120
+      #:font_size? 'small
+      (lambda ()
+        (detail-h1 "Decode XML Process")
+        (detail-line (format "XML File:~a" xml))
+        (detail-line (format "List Symbol:~a" sym_list))
 
-          (map (lambda (sym) (hash-set! sym_hash sym #f)) sym_list)
+        (call-with-input-file
+            xml
+          (lambda (origin_port)
+            ;; remove all the spaces
+            (call-with-input-string
+             (regexp-replace* #rx"> *<"
+                              (regexp-replace* #rx"\n|\r" (port->string origin_port) "")
+                              "><")
+             (lambda (filtered_port)
+               (let ([xml_hash (make-hash)]
+                     [sym_hash (make-hash)]
+                     )
 
-          (let loop-node ([ancester_prefix ""]
-                          [xml_list (list (xml->xexpr (document-element (read-xml filtered_port))))])
-            
-;;            (printf "~a\n" xml_list)
+                 ;; which syms need be count and convert to list, turn them to map to convenient to check
+                 (map (lambda (sym) (hash-set! sym_hash sym #f)) sym_list)
 
-            (when (not (null? xml_list))
-                  (if (not (list? (car xml_list)))
-                      (if (hash-has-key? sym_hash ancester_prefix)
-                          (let ([count_sym (format "~a.count" ancester_prefix)])
-                            (hash-set! xml_hash count_sym (add1 (hash-ref xml_hash count_sym 0)))
-                            (hash-set! xml_hash (format "~a~a" ancester_prefix (hash-ref xml_hash count_sym)) (car xml_list)))
-                          (hash-set! xml_hash ancester_prefix (car xml_list)))
-                      (let* ([node (car xml_list)]
-                             [prefix (car node)]
-                             [attr_list (cadr node)]
-                             [content_list (cddr node)])
-                        
-                        (let loop-attr ([attrs attr_list])
-                          (when (not (null? attrs))
-                                (when (hash-has-key? sym_hash prefix)
-                                      (let ([count_sym (format "~a.count" prefix)])
-                                        (hash-set! xml_hash count_sym (add1 (hash-ref xml_hash count_sym 0)))
-                                        (set! prefix (format "~a~a" prefix (hash-ref xml_hash count_sym)))))
-                                (hash-set! xml_hash (format "~a.~a" prefix (caar attrs)) (cadar attrs))
-                                (loop-attr (cdr attrs))))
+                 ;; load xml file to xexpr to start parsing
+                 ;; ancester_prefix means parent node name, start from ""
+                 (let loop-node ([ancester_prefix ""]
+                                 [in_list? #f]
+                                 [xml_list (list (xml->xexpr (document-element (read-xml filtered_port))))])
 
-                        (if (null? content_list)
-                            (loop-node prefix '(""))
-                            (loop-node prefix content_list))
-                        
-                        (loop-node prefix (cdr xml_list))))))
-          xml_hash))))))
+                   (detail-line "")
+                   (detail-line "***************************************")
+                   (detail-line (format "ancester_prefix:[~a]\n" ancester_prefix))
+                   (detail-line (format "in_list?;[~a]\n" in_list?))
+                   (detail-line (format "xml_list:[~a]\n" xml_list))
+                   (detail-line "***************************************")
+
+                   (if (not (null? xml_list))
+                       (let ([node (car xml_list)])
+                         (detail-line (format "node:[~a]" node))
+                         (if (not (list? node))
+                             (begin
+                               (detail-line "node is a symbol")
+                               (if (hash-has-key? sym_hash ancester_prefix) ;; prefix is the sym we expect it to be a list, count it
+                                   (begin
+                                     (detail-line "node is a list symbol")
+                                     (hash-set! xml_hash (format "~a.count" ancester_prefix) (add1 (hash-ref xml_hash (format "~a.count" ancester_prefix) 0)))
+                                     (detail-line (format "xml_hash after process attrs:[~a]" xml_hash))
+                                     (loop-node node #t (cdr xml_list)))
+                                   (begin
+                                     (detail-line "node is not a list symbol")
+                                     (loop-node node #f (cdr xml_list)))))
+                             (let* ([prefix (car node)]
+                                    [attr_list (cadr node)]
+                                    [content_list (cddr node)])
+
+                               (detail-line "node is a list")
+                               (detail-line (format "prefix:[~a]" prefix))
+                               (detail-line (format "attrs:[~a]" attr_list))
+                               (detail-line (format "content:[~a]" content_list))
+
+                               (detail-line "process the attrs")
+                               (let loop-attr ([attrs attr_list])
+                                 (when (not (null? attrs))
+                                       (when (hash-has-key? sym_hash ancester_prefix)
+                                             (let ([count_sym (format "~a.~a.count" ancester_prefix prefix)])
+                                               (hash-set! xml_hash count_sym (add1 (hash-ref xml_hash count_sym 0)))
+                                               (set! prefix (format "~a~a" prefix (hash-ref xml_hash count_sym)))))
+                                       (hash-set! xml_hash (format "~a.~a.~a" ancester_prefix prefix (caar attrs)) (cadar attrs))
+                                       (loop-attr (cdr attrs))))
+                               (detail-line (format "xml_hash after process attrs:[~a]" xml_hash))
+
+                               (detail-line "process the content")
+                               (if (null? content_list)
+                                   (loop-node prefix in_list? '(""))
+                                   (loop-node prefix in_list? content_list))
+
+                               (loop-node prefix in_list? (cdr xml_list)))))
+                       xml_hash))))))))))))
--- simple-xlsx/tests/lib/xml/xml-test.rkt
+++ simple-xlsx/tests/lib/xml/xml-test.rkt
@@ -17,7 +17,8 @@
    (test-case
     "test-workbook"
 
-    (let ([xml_hash (load-xml-hash workbook_xml_file '(sheet))])
+    (let ([xml_hash (load-xml-hash workbook_xml_file '(sheets))])
+      (printf "~a\n" xml_hash)
       (check-equal? (hash-count xml_hash) 58)
       (check-equal? (hash-ref xml_hash "workbook.xmlns") "http://schemas.openxmlformats.org/spreadsheetml/2006/main")
       (check-equal? (hash-ref xml_hash "fileVersion.appName") "xl")
@@ -37,38 +38,38 @@
 
     )
 
-   (test-case
-    "test-shared-string"
-
-    (let ([xml_hash (load-xml-hash sharedStrings_xml_file '(t phoneticPr))])
-      (check-equal? (hash-count xml_hash) 73)
-      (check-equal? (hash-ref xml_hash "sst.count") "17")
-      (check-equal? (hash-ref xml_hash "sst.uniqueCount") "17")
-
-      (check-equal? (hash-ref xml_hash "t.count") 17)
-      (check-equal? (hash-ref xml_hash "phoneticPr.count") 17)
-
-      (check-equal? (hash-ref xml_hash "t1") "")
-      (check-equal? (hash-ref xml_hash "t2") "201601")
-      (check-equal? (hash-ref xml_hash "t10") "Center")
-      (check-equal? (hash-ref xml_hash "t17") "month/brand")
-      )
-    )
-   
-   (test-case
-    "test-load-sheet"
-    
-    (let (
-          [xml_hash (load-xml-hash sheet_xml_file '(col row c))]
-          )
-      
-      (printf "~a\n" xml_hash)
-
-      (check-equal? (hash-count xml_hash) 60)
-      (check-equal? (hash-ref xml_hash "col.count") 4)
-      (check-equal? (hash-ref xml_hash "row.count") 4)
-
-      ))
+;   (test-case
+;    "test-shared-string"
+;
+;    (let ([xml_hash (load-xml-hash sharedStrings_xml_file '(t phoneticPr))])
+;      (check-equal? (hash-count xml_hash) 73)
+;      (check-equal? (hash-ref xml_hash "sst.count") "17")
+;      (check-equal? (hash-ref xml_hash "sst.uniqueCount") "17")
+;
+;      (check-equal? (hash-ref xml_hash "t.count") 17)
+;      (check-equal? (hash-ref xml_hash "phoneticPr.count") 17)
+;
+;      (check-equal? (hash-ref xml_hash "t1") "")
+;      (check-equal? (hash-ref xml_hash "t2") "201601")
+;      (check-equal? (hash-ref xml_hash "t10") "Center")
+;      (check-equal? (hash-ref xml_hash "t17") "month/brand")
+;      )
+;    )
+;   
+;   (test-case
+;    "test-load-sheet"
+;    
+;    (let (
+;          [xml_hash (load-xml-hash sheet_xml_file '(row))]
+;          )
+;      
+;      (printf "~a\n" xml_hash)
+;
+;      (check-equal? (hash-count xml_hash) 60)
+;      (check-equal? (hash-ref xml_hash "col.count") 4)
+;      (check-equal? (hash-ref xml_hash "row.count") 4)
+;
+;      ))
   ))
 
 (run-tests test-xml)
