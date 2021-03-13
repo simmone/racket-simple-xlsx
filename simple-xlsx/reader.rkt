d0c46f76bf45bef461e655aa9ec3975f7b7e7583
Chen Xiao <chenxiao770117@gmail.com>
2021-03-13 09:35:31 +0800

    update

--- simple-xlsx/reader.rkt
+++ simple-xlsx/reader.rkt
@@ -24,25 +24,14 @@
   (call-with-unzip
    xlsx_file
    (lambda (tmp_dir)
-     (let ([_xlsx (new-xlsx)])
+     (let ([_xlsx (new-xlsx tmp_dir)])
        (load-workbook (build-path tmp_dir "xl" "workbook.xml") _xlsx)
 
        (load-shared-strings (build-path tmp_dir "xl" "sharedStrings.xml") _xlsx)
 
-       (set! read_sheet_rid_rel_map (load-workbook-rels (build-path tmp_dir "xl" "_rels" "workbook.xml.rels")))
+       (load-workbook-rels (build-path tmp_dir "xl" "_rels" "workbook.xml.rels") _xlsx)
        
-       (set! xlsx_obj
-             (new new-xlsx%
-                  (xlsx_dir tmp_dir)
-                  (sheet_id_list read_sheet_id_list)
-                  (sheet_id_name_map read_sheet_id_name_map)
-                  (sheet_name_id_map read_sheet_name_id_map)
-                  (sheet_id_rid_map read_sheet_id_rid_map)
-                  (sheet_rid_rel_map read_sheet_rid_rel_map)
-                  (shared_strings_map read_shared_strings_map)
-                  (sheet #f)))
-
-       (user_proc xlsx_obj)))))
+       (user_proc _xlsx)))))
 
 (define (sheet-name-rows xlsx_file_path sheet_name)
   (with-input-from-xlsx-file
