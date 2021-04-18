a9bc340120d685eca636d7c2712b407fa7da7a99
Chen Xiao <chenxiao770117@gmail.com>
2021-04-18 09:52:14 +0800

    update

--- simple-xlsx/new/docProps/docprops-app.rkt
+++ simple-xlsx/new/docProps/docprops-app.rkt
@@ -17,33 +17,27 @@
 (define S string-append)
 
 (define (print-sheet-variant)
-  (let ([sheet_type_count_map (make-hash)])
-    (let loop ([loop_list sheet_list])
-      (when (not (null? loop_list))
-           (hash-set! sheet_type_count_map (sheet-type (car loop_list)) (add1 (hash-ref sheet_type_count_map (sheet-type (car loop_list)) 0)))
-           (loop (cdr loop_list))))
+  (with-output-to-string
+    (lambda ()
+      (printf "<HeadingPairs><vt:vector size=\"~a\" baseType=\"variant\">" (length (XLSX-sheet_list (*CURRENT_XLSX*))))
 
-    (with-output-to-string
-      (lambda ()
-        (printf "<HeadingPairs><vt:vector size=\"~a\" baseType=\"variant\">" (* (hash-count sheet_type_count_map) 2))
-        (for-each
-         (lambda (type_count)
-           (cond
-            [(eq? (car type_count) 'data)
-             (printf "<vt:variant><vt:lpstr>工作表</vt:lpstr></vt:variant><vt:variant><vt:i4>~a</vt:i4></vt:variant>" (cdr type_count))]
-            [(eq? (car type_count) 'chart)
-             (printf "<vt:variant><vt:lpstr>图表</vt:lpstr></vt:variant><vt:variant><vt:i4>~a</vt:i4></vt:variant>" (cdr type_count))]
-            ))
-         (sort (hash->list sheet_type_count_map) (lambda (c d) (string>? (symbol->string c) (symbol->string d)))  #:key car))))))
+      (let ([data_sheet_count (length (filter (lambda (sheet) (DATA-SHEET? sheet)) (XLSX-sheet_list (*CURRENT_XLSX*))))])
+        (when (> data_sheet_count 0)
+              (printf "<vt:variant><vt:lpstr>工作表</vt:lpstr></vt:variant><vt:variant><vt:i4>~a</vt:i4></vt:variant>" data_sheet_count)))
+
+      (let ([chart_sheet_count (length (filter (lambda (sheet) (CHART-SHEET? sheet)) (XLSX-sheet_list (*CURRENT_XLSX*))))])
+        (when (> chart_sheet_count 0)
+           (printf "<vt:variant><vt:lpstr>图表</vt:lpstr></vt:variant><vt:variant><vt:i4>~a</vt:i4></vt:variant>" chart_sheet_count))))))
  
 (define (write-docprops-app) @S{
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes"><Application>Microsoft Excel</Application><DocSecurity>0</DocSecurity><ScaleCrop>false</ScaleCrop>@|(print-sheet-variant sheet_list)|</vt:vector></HeadingPairs><TitlesOfParts><vt:vector size="@|(number->string (length sheet_list))|" baseType="lpstr">@|(with-output-to-string
+<Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes"><Application>Microsoft Excel</Application><DocSecurity>0</DocSecurity><ScaleCrop>false</ScaleCrop>@|(print-sheet-variant sheet_list)|</vt:vector></HeadingPairs><TitlesOfParts><vt:vector size="@|(number->string (length (XLSX-sheet_list (*CURRENT_XLSX*))))|" baseType="lpstr">@|(with-output-to-string
   (lambda ()
-    (for-each
-      (lambda (sheet)
-        (printf "<vt:lpstr>~a</vt:lpstr>" (sheet-name sheet)))
-     sheet_list)))|</vt:vector></TitlesOfParts><LinksUpToDate>false</LinksUpToDate><SharedDoc>false</SharedDoc><HyperlinksChanged>false</HyperlinksChanged><AppVersion>12.0000</AppVersion></Properties>
+    (let loop ([sheets (XLSX-sheet_list (*CURRENT_XLSX*))]
+               [index 0])
+      (when (not (null? sheets))
+        (printf "<vt:lpstr>~a</vt:lpstr>" (hash-ref (XLSX-sheet_index_name_map (*CURRENT_XLSX*)) index))
+        (loop (cdr sheets) (add1 index))))))|</vt:vector></TitlesOfParts><LinksUpToDate>false</LinksUpToDate><SharedDoc>false</SharedDoc><HyperlinksChanged>false</HyperlinksChanged><AppVersion>12.0000</AppVersion></Properties>
 })
 
 (define (write-docprops-app-file)
