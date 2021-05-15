ee9e18b99fece8d3eea3b943f1ac9ac4fcc793d1
Chen Xiao <chenxiao770117@gmail.com>
2021-05-15 23:38:04 +0800

    update

--- simple-xlsx/new/docProps/docprops-core.rkt
+++ simple-xlsx/new/docProps/docprops-core.rkt
@@ -2,26 +2,32 @@
 
 (require simple-xml)
 
-(require racket/file)
-(require racket/port)
-(require racket/list)
-(require racket/contract)
-
 (require "../../xlsx/xlsx.rkt")
 (require "../../sheet/sheet.rkt")
 (require "../../lib/lib.rkt")
 
 (provide (contract-out
-          [write-docprops-core (-> date? string?)]
+          [docprops-core (-> date? list?)]
           [write-docprops-core-file (-> date? void?)]
+          [read-docpros-core (-> void?)]
           ))
 
-(define S string-append)
- 
-(define (write-docprops-core the_date) @S{
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><dc:creator></dc:creator><cp:lastModifiedBy></cp:lastModifiedBy><dcterms:created xsi:type="dcterms:W3CDTF">2006-09-13T11:21:51Z</dcterms:created><dcterms:modified xsi:type="dcterms:W3CDTF">@|(format-w3cdtf the_date)|</dcterms:modified></cp:coreProperties>
-})
+(define (docprops-core the_date)
+  (list
+   "cp:coreProperties"
+   (cons "xmlns:cp" "http://schemas.openxmlformats.org/package/2006/metadata/core-properties")
+   (cons "xmlns:dc" "http://purl.org/dc/elements/1.1/")
+   (cons "xmlns:dcterms" "http://purl.org/dc/terms/")
+   (cons "xmlns:dcmitype" "http://purl.org/dc/dcmitype/")
+   (cons "xmlns:xsi" "http://www.w3.org/2001/XMLSchema-instance")
+   (list "dc:creator" "")
+   (list "cp:lastModifiedBy" "")
+   (list "dcterms:created"
+         (cons "xsi:type" "dcterms:W3CDTF")
+         (format-w3cdtf the_date))
+   (list "dcterms:modified"
+         (cons "xsi:type" "dcterms:W3CDTF")
+         (format-w3cdtf the_date))))
 
 (define (write-docprops-core-file the_date)
   (let ([docprops_dir (build-path (XLSX-xlsx_dir (*CURRENT_XLSX*)) "docProps")])
@@ -30,4 +36,7 @@
     (with-output-to-file (build-path docprops_dir "core.xml")
       #:exists 'replace
       (lambda ()
-        (printf "~a" (write-docprops-core the_date))))))
+        (printf "~a" (lists->compact_xml (docprops-core)))))))
+
+(define (read-docpros-core)
+  (void))
